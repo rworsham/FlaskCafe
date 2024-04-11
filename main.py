e9254f665acdf76337d7b2070ca9ea70d844759e
rworsham <robertworsham01@gmail.com>
2024-04-10 22:25:14 -0400

    Added new page for adding cafe, redesigned Nav Bar on all 3 pages, Added FlaskWtf Forms and was working until added choices to select field will tackle next

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, url_for, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, EmailField, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -37,21 +37,46 @@ class Cafe(db.Model):
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
 
+class CreateCafeForm(FlaskForm):
+    name = StringField("Cafe Name", validators=[DataRequired()])
+    map_url = StringField("Google Maps Location Link", validators=[DataRequired(), URL()])
+    img_url = StringField("Cafe Image URL", validators=[DataRequired(), URL()])
+    location = StringField("Location Name", validators=[DataRequired()])
+    seats = StringField("Number Of Seats", validators=[DataRequired()])
+    has_toilet = SelectField("Are there restrooms available?",choices=[(0, 'No'),(1,"Yes")], validators=[DataRequired()])
+    has_wifi = SelectField("Does this Cafe offer Wifi?",choices=[(0, 'No'),(1,"Yes")], validators=[DataRequired()])
+    has_sockets = SelectField("Are there power outlets available?",choices=[(0, 'No'),(1,"Yes")], validators=[DataRequired()])
+    can_take_calls = SelectField("Can you take phone calls?",choices=[(0, 'No'),(1,"Yes")], validators=[DataRequired()])
+    coffee_price = StringField("Estimated price of coffee.",choices=[(0, 'No'),(1,"Yes")], validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
 @app.route("/")
 def home():
     all_cafes = Cafe.query.order_by(Cafe.id).all()
     cafe_list = [all_cafes]
     return render_template('index.html', cafes=cafe_list)
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add_cafe():
-    pass
-
-
-@app.route("/delete")
-def delete_cafe():
-    pass
-
+    form = CreateCafeForm()
+    if form.validate_on_submit():
+        new_cafe = Cafe(
+            name=form.name.data,
+            map_url=form.map_url.data,
+            img_url=form.img_url.data,
+            location=form.location.data,
+            seats=form.location.data,
+            has_toilet=form.has_toilet.data,
+            has_wifi=form.has_wifi.data,
+            has_sockets=form.has_sockets.data,
+            can_take_calls=form.can_take_calls.data,
+            coffee_price=form.coffee_price.data
+        )
+        db.session.add(new_cafe)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add.html', form=form)
 
 @app.route("/<int:id>")
 def show_cafe(id):
