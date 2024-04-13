991ec169613a7f9870c07122e45d9f063807606e
rworsham <robertworsham01@gmail.com>
2024-04-13 00:17:28 -0400

    Corrected boolean forms and implemented authentication and two supporting pages, tidy up req

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, BooleanField, EmailField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 import smtplib
 from email.message import EmailMessage
 
 app = Flask(__name__)
+app.secret_key = "rob"
+login_manager = LoginManager()
 
 
 class Base(DeclarativeBase):
@@ -19,7 +23,7 @@ class Base(DeclarativeBase):
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-app.secret_key = "rob"
+login_manager.init_app(app)
 bootstrap = Bootstrap5(app)
 
 
@@ -37,27 +41,109 @@ class Cafe(db.Model):
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
 
+class User(UserMixin, db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
+
+
 class CreateCafeForm(FlaskForm):
     name = StringField("Cafe Name", validators=[DataRequired()])
     map_url = StringField("Google Maps Location Link", validators=[DataRequired(), URL()])
     img_url = StringField("Cafe Image URL", validators=[DataRequired(), URL()])
     location = StringField("Location Name", validators=[DataRequired()])
     seats = StringField("Number Of Seats", validators=[DataRequired()])
-    has_toilet = SelectField("Are there restrooms available?",choices=[(0, 'No'),(1,"Yes")], validators=[DataRequired()])
-    has_wifi = SelectField("Does this Cafe offer Wifi?",choices=[(0, 'No'),(1,"Yes")], validators=[DataRequired()])
-    has_sockets = SelectField("Are there power outlets available?",choices=[(0, 'No'),(1,"Yes")], validators=[DataRequired()])
-    can_take_calls = SelectField("Can you take phone calls?",choices=[(0, 'No'),(1,"Yes")], validators=[DataRequired()])
-    coffee_price = StringField("Estimated price of coffee.",choices=[(0, 'No'),(1,"Yes")], validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
+    has_toilet = BooleanField("Are there restrooms available?")
+    has_wifi = BooleanField("Does this Cafe offer Wifi?")
+    has_sockets = BooleanField("Are there power outlets available?")
+    can_take_calls = BooleanField("Can you take phone calls?")
+    coffee_price = StringField("Estimated price of coffee.", validators=[DataRequired()])
+    submit = SubmitField("Add Cafe")
+
+
+class RegistrationForm(FlaskForm):
+    email = EmailField("Email Address", validators=[DataRequired()])
+    password = PasswordField("Enter Password", validators=[DataRequired()])
+    name = StringField("Username", validators=[DataRequired()])
+    submit = SubmitField("Sign Up")
+
+
+class LoginForm(FlaskForm):
+    email = EmailField("Email Address", validators=[DataRequired()])
+    password = PasswordField("Enter Password", validators=[DataRequired()])
+    submit = SubmitField("Sign In")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route('/register',methods=["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user_email = result.scalar()
+        name = form.name.data
+        name_result = db.session.execute(db.select(User).where(User.name == name))
+        user_name = name_result.scalar()
+        if user_email:
+            flash("Youve already signed up with that email, login instead")
+            return redirect(url_for('login'))
+        new_user = User(
+            name=form.name.data,
+            email=form.email.data,
+            password=generate_password_hash(password=form.password.data,method="pbkdf2:sha256",salt_length=8)
+        )
+        if user_name:
+            message = "Username already exist, please try again"
+            return render_template("register.html", messages=message)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('home'))
+    return render_template("register.html", form=form)
+
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        if not user:
+            flash("That email does not exist, please try again")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash("Password Incorrect. Please try again")
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('home'))
+    return render_template("login.html", form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route("/")
 def home():
     all_cafes = Cafe.query.order_by(Cafe.id).all()
     cafe_list = [all_cafes]
-    return render_template('index.html', cafes=cafe_list)
+    return render_template('index.html', cafes=cafe_list, current_user=current_user)
+
 
 @app.route("/add", methods=["GET", "POST"])
+@login_required
 def add_cafe():
     form = CreateCafeForm()
     if form.validate_on_submit():
@@ -76,7 +162,16 @@ def add_cafe():
         db.session.add(new_cafe)
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template('add.html', form=form)
+    return render_template('add.html', form=form, current_user=current_user)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    req_cafe = db.get_or_404(Cafe, id)
+    db.session.delete(req_cafe)
+    db.session.commit()
+    return redirect(url_for('home'))
+
 
 @app.route("/<int:id>")
 def show_cafe(id):
